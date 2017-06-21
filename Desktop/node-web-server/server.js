8cdfd6bd478e7a6dc5a5b4b27387832b56b8c082
cma43 <cmartman@ncsu.edu>
2017-06-20 22:02:04 -0400

    The up and running prototype of our web page using Annotorious (finally)

--- Desktop/node-web-server/server.js
+++ Desktop/node-web-server/server.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+// const anno = require('angular-annotorious');
+const http = require('http');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
 
 app.use((req, res, next) => {
  var now = new Date().toString();
@@ -24,41 +26,38 @@ next();
 });
 
 
-app.use((req,res, next) => {
-	res.render('login.hbs');
-});
+// app.use((req,res, next) => {
+// 	res.render('login.hbs');
+// });
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 
-hbs.registerHelper('getCurrentYear', () => {
-	return new Date().getFullYear()
-});
+// hbs.registerHelper('getCurrentYear', () => {
+// 	return new Date().getFullYear()
+// });
 
-hbs.registerHelper('screamIt', (text) => {
-	return text.toUpperCase();
-});
+// hbs.registerHelper('screamIt', (text) => {
+// 	return text.toUpperCase();
+// });
 
 app.get('/', (req, res) => { 
-res.render('home.hbs', {
-	pageTitle: 'Home Page',
-	welcomeMessage: 'Welcome to my website',
-});
+res.render('example.hbs');
 });
 
-app.get('/about', (req,res) => {
-	res.render('about.hbs', {
-		pageTitle: 'About Page',
-	});
-});
+// app.get('/about', (req,res) => {
+// 	res.render('about.hbs', {
+// 		pageTitle: 'About Page',
+// 	});
+// });
 
 
-app.get('/bad', (req,res) => {
-	res.send({
-		errorMessage: 'Unable to handle request!'
-	});
-});
+// app.get('/bad', (req,res) => {
+// 	res.send({
+// 		errorMessage: 'Unable to handle request!'
+// 	});
+// });
 
 
 app.listen(port, () => {
